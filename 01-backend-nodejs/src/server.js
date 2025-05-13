e3561e0dde81d215ace8e726985cbd5609fa461b
Pham Hung <hungk77777@gmail.com>
2025-05-13 21:52:31 +0700

    test

--- 01-backend-nodejs/src/server.js
+++ 01-backend-nodejs/src/server.js
@@ -1,20 +1,32 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
-import configViewEngine from "./configs/viewEngine";
-import initWebRoutes from "./routes/web";
+import configViewEngine from "./configs/viewEngine.js";
+import initWebRoutes from "./routes/web.js";
 
 // init 
 const app = express();
+const routerAPI = express.Router();
+
+// config body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // config view engine
 configViewEngine(app);
 
+// API route
+routerAPI.get("/api", (req, res) => {
+    res.json({ message: "Hello from API" });
+});
+app.use("/v1/api", routerAPI);
 
-// init web routes
+// web routes
 initWebRoutes(app);
 
-
-const PORT = process.env.POR || 8080;
-app.listen(PORT, () => {
-    console.log(">>> JWT Backend is running on the port " + PORT);
-})
+// start server
+(async () => {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(">>> JWT Backend is running on port " + process.env.PORT);
+    });
+})();
