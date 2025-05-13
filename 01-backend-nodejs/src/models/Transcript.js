7adda40f4ce41ff296e53fc5bb1cc72ee61bff59
Thế Pháp <phapok0@gmail.com>
2025-05-13 23:11:36 +0700

    fix

--- 01-backend-nodejs/src/models/Transcript.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const transcriptSchema = new mongoose.Schema({
-  transcript: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Transcript', transcriptSchema);
--- 01-backend-nodejs/src/models/submission.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const recordSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  totalTests: { type: Number, default: 0 },
-  averageScore: { type: Number, default: 0 }
-});
-
-export default mongoose.model("Record", recordSchema);
