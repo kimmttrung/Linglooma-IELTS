b7b02e992d4d4229ff88ae252fdf3c7bc405eef2
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-05-14 11:34:01 +0700

    design sidebar and dashboard header

---
+++ 00-fontend-react/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,28 @@
+import Sidebar from "../Admin/Sidebar";
+import DashboardHeader from "./Dashboard-header";
+
+
+const Dashboard = () => {
+    return (
+        <div className="flex h-screen bg-gray-50">
+            <Sidebar />
+            <div className="flex-1 overflow-y-auto">
+                <div className="container mx-auto p-6">
+                    <DashboardHeader />
+                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6">
+                        <div className="lg:col-span-2">
+                            {/* <DaySelector />
+                            <StudyPlanner /> */}
+                        </div>
+                        <div className="space-y-6">
+                            {/* <Calendar />
+                            <RecentActivity /> */}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Dashboard;
